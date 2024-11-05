e6cb2723f7ba1f991378c6bfddfb525ced78c518
Suryavir Kapur <suryavirkapur@hotmail.com>
2024-11-06 00:04:18 +0530

    XXX

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
-  trustedOrigins: ["http://localhost:3000"],
+  trustedOrigins: ["http://localhost:3000", "https://plgrzr.suryavirkapur.com"],
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
